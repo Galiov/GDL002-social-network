d904ee33404806e8012f9a90a6c2b3517e338075
GalioV <galilea.valle@gmail.com>
2019-03-25 15:39:40 -0600

    Agregamos node_modules/ a gitignore

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const registrar = () => {
+	let email = document.getElementById("mail").value;
+	let password = document.getElementById("password").value;
+
+	console.log(email);
+	console.log(password);
+	
+};
