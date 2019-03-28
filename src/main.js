0baf75396bfed61172b94d895e8e9b932d23a16b
GalioV <galilea.valle@gmail.com>
2019-03-28 00:21:24 -0600

    Agregamos configuraciones de prettier

--- src/main.js
+++ src/main.js
@@ -1,127 +1,132 @@
-
 const showSignUp = () => {
-    const signUpForm = document.querySelector(".sign-up");
-    const signInForm = document.querySelector(".sign-in");
+	const signUpForm = document.querySelector('.sign-up');
+	const signInForm = document.querySelector('.sign-in');
 
-    signUpForm.style.display = 'block';
-    signInForm.style.display = 'none';
-}
-const signUpButton = document.querySelector(".sign-up-button");
-signUpButton.addEventListener("click", showSignUp);
+	signUpForm.style.display = 'block';
+	signInForm.style.display = 'none';
+};
+const signUpButton = document.querySelector('.sign-up-button');
+signUpButton.addEventListener('click', showSignUp);
 
 //Funcion para registrar usuarios nuevos
 const register = () => {
-	let email = document.querySelector(".mailSignUp").value;
-	let password = document.querySelector(".passwordSignUp").value;
+	let email = document.querySelector('.mailSignUp').value;
+	let password = document.querySelector('.passwordSignUp').value;
 
 	console.log(email);
 	console.log(password);
 
-	firebase.auth().createUserWithEmailAndPassword(email, password)
-	.then(function(){
-		verification()
-	})
-	.catch(function(error) {
-		// Handle Errors here.
-		let errorCode = error.code;
-		let errorMessage = error.message;
-		// ...
-		alert(errorMessage);
-		console.log(errorMessage);
-	  });
-}
-const registerButton = document.querySelector(".register-button");
-registerButton.addEventListener("click", register);
-
+	firebase
+		.auth()
+		.createUserWithEmailAndPassword(email, password)
+		.then(function() {
+			verification();
+		})
+		.catch(function(error) {
+			// Handle Errors here.
+			let errorCode = error.code;
+			let errorMessage = error.message;
+			// ...
+			alert(errorMessage);
+			console.log(errorMessage);
+		});
+};
+const registerButton = document.querySelector('.register-button');
+registerButton.addEventListener('click', register);
 
 //Funcion para entrar a los usuarios ya registrados
 const enter = () => {
-    let emailSignIn = document.querySelector(".mail").value;
-    let passwordSignIn = document.querySelector(".password").value;
+	let emailSignIn = document.querySelector('.mail').value;
+	let passwordSignIn = document.querySelector('.password').value;
 
-    console.log(emailSignIn);
-    console.log(passwordSignIn);
+	console.log(emailSignIn);
+	console.log(passwordSignIn);
 
-    firebase.auth().signInWithEmailAndPassword(emailSignIn, passwordSignIn)
-        .catch(function(error) {
-            // Handle Errors here.
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            // ...
-            alert(errorMessage);
-            console.log(errorMessage);
-        });
-}
+	firebase
+		.auth()
+		.signInWithEmailAndPassword(emailSignIn, passwordSignIn)
+		.catch(function(error) {
+			// Handle Errors here.
+			let errorCode = error.code;
+			let errorMessage = error.message;
+			// ...
+			alert(errorMessage);
+			console.log(errorMessage);
+		});
+};
 
-const signInButton = document.querySelector(".sign-in-button");
-signInButton.addEventListener("click", enter);
+const signInButton = document.querySelector('.sign-in-button');
+signInButton.addEventListener('click', enter);
 
 //Funcion para verificar el correo electronico del usuario
 const verification = () => {
-    let user = firebase.auth().currentUser;
-    user.sendEmailVerification().then(function() {
-        // Email sent.
-        alert("Te hemos enviado un código de verificación, por favor revisa tu bandeja para poder ingresar");
-        console.log("Enviando correo")
-
-    }).catch(function(error) {
-        // An error happened.
-    });
-}
+	let user = firebase.auth().currentUser;
+	user
+		.sendEmailVerification()
+		.then(function() {
+			// Email sent.
+			alert(
+				'Te hemos enviado un código de verificación, por favor revisa tu bandeja para poder ingresar',
+			);
+			console.log('Enviando correo');
+		})
+		.catch(function(error) {
+			// An error happened.
+		});
+};
 
 //Funcion para observar todo lo que esta haciendo el codigo, registro, entrada, salida, usuario, etc.
 const observador = () => {
-
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            console.log("Existe Usuario activo");
-            showContent(user);
-            // User is signed in.
-            let displayName = user.displayName;
-            let email = user.email;
-            console.log(user);
-            console.log(user.emailVerified);
-            let emailVerified = user.emailVerified;
-            let photoURL = user.photoURL;
-            let isAnonymous = user.isAnonymous;
-            let uid = user.uid;
-            let providerData = user.providerData;
-            // ...
-        } else {
-            // User is signed out.
-            console.log("No existe usuario activo");
-                // ...
-        }
-    });
-}
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			console.log('Existe Usuario activo');
+			showContent(user);
+			// User is signed in.
+			let displayName = user.displayName;
+			let email = user.email;
+			console.log(user);
+			console.log(user.emailVerified);
+			let emailVerified = user.emailVerified;
+			let photoURL = user.photoURL;
+			let isAnonymous = user.isAnonymous;
+			let uid = user.uid;
+			let providerData = user.providerData;
+			// ...
+		} else {
+			// User is signed out.
+			console.log('No existe usuario activo');
+			// ...
+		}
+	});
+};
 observador();
 
-
 //Funcion que muestra contenido a los usuarios registrados
-const showContent = (user) => {
-    let user1 = user;
-    let content = document.querySelector(".content");
-    if (user1.emailVerified) {
-        content.innerHTML = `
+const showContent = user => {
+	let user1 = user;
+	let content = document.querySelector('.content');
+	if (user1.emailVerified) {
+		content.innerHTML = `
 		<p>Welcome to WoTravel!</p>
 		<button class="sign-out-button">Sign Out</button>
 		`;
-        const signOutButton = document.querySelector(".sign-out-button");
-        signOutButton.addEventListener("click", close);
-    }
-
-}
+		const signOutButton = document.querySelector('.sign-out-button');
+		signOutButton.addEventListener('click', close);
+	}
+};
 
 //Funcion de boton para cerrar sesion
 const close = () => {
-    firebase.auth().signOut()
-        .then(function() {
-            console.log("Saliendo... :)")
-        })
-        .catch(function(error) {
-            console.log(error)
-        })
-}
+	firebase
+		.auth()
+		.signOut()
+		.then(function() {
+			console.log('Saliendo... :)');
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+};
 
 /*	  let verificationMessage = document.querySelector(".sign-up");
 	  content.innerHTML = `
@@ -133,16 +138,16 @@ const close = () => {
 let db = firebase.firestore();
 //agregar informacion
 function post() {
-    let posts = document.querySelector(".post").value;
-    db.collection("userPost").add({
-            text: posts
-        })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-            document.querySelector(".post").value = "";
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
-
+	let posts = document.querySelector('.post').value;
+	db.collection('userPost')
+		.add({
+			text: posts,
+		})
+		.then(function(docRef) {
+			console.log('Document written with ID: ', docRef.id);
+			document.querySelector('.post').value = '';
+		})
+		.catch(function(error) {
+			console.error('Error adding document: ', error);
+		});
 }
